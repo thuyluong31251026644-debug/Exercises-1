3ef5b35593f2288d2f80f998ababea00252812c6
thuyluong31251026644-debug <thuyluong.31251026644@st.ueh.edu.vn>
2026-04-23 12:39:25 +0700

    Add files via upload

---
+++ baitap_8.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double r = 5;
+        double area = Math.PI * r * r;
+        Console.WriteLine("Area = " + area);
+    }
+}
